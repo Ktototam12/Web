b4aca0448fb149bde405ccd3ce56c5d47fc47ce2
Анна Кирпа <70509092+Ktototam12@users.noreply.github.com>
2021-04-28 01:18:54 +0500

    Add files via upload

--- mySite/www/index.php
+++ mySite/www/index.php
@@ -1,65 +1,53 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8" />
-  <title> Фильмы и сериалы</title>
-  <link href="/css/style.css" rel="stylesheet" type="text/css">
+  <?php 
+    $title = "Фильмы и сериалы";
+    require_once "blocks/head.php" ;
+  ?>
 </head>
 <body>
-   <header>
-      <div id="logo">
-        <a href="/" title="Перейти на главную">
-           <span>Н</span>овости <span>К</span>ино
-        </a>
-      </div>
-      <div id="menuHead">
-        <a href="/about.php">
-          <div style="margin-right: 5%">О нас</div>
-        </a>
-        <a href="/feedback.php">
-          <div>Обратная связь</div>  
-        </a>
-      </div>
-      <div id="regAuth">
-      <a href="/reg.php">Регистрация</a> | <a href="auth.php"> Авторизация</a>
-      </div>
-   </header>
+   <?php require_once "blocks/header.php" ?>
    
    <div id="wrapper">
 		<div id="leftCol">
-		
+       <div id="bigArticle">
+         <img src="/img/articles/1.png" alt="Статья 1" title="Статья 1">
+         <h2>Новинки кино</h2>
+         <p>В этой статье (а она будет постоянно обновляться) мы расскажем вам о самых лучших новых фильмах
+            и мультфильмах 2020 и 2021 года, которые уже вышли в хорошем качестве и которые можно посмотреть
+             в онлайн-кинотеатрах или же скачать на торрент-трекерах (но естественно только для ознакомления).</p>
+          <a href="/article.php">
+           <div class="more">Далее</div>
+          </a>
+       </div>
+       <div class="clear"><br></div>
+       <div class="article">
+           <img src="/img/articles/2.jpg" alt="Статья 1" title="Статья 1">
+           <h2>Самые ожидаемые фильмы</h2>
+           <p>В этой статье (а она будет постоянно обновляться) мы расскажем вам о самых ожидаемых предстоящих фильмах, 
+             которые еще только выйдут на экраны российских кинотеатров, с их главными подробностями, описанием сюжета, 
+             датой премьеры и трейлерами на русском.</p>
+           <a href="/article.php">
+             <div class="more">Далее</div>
+           </a>
+       </div>
+       <div class="article">
+           <img src="/img/articles/3.jpg" alt="Статья 1" title="Статья 1">
+           <h2>Календарь выхода фильмов</h2>
+           <p>На многих киносайтах можно найти подробное расписание выхода тех или иных фильмов, но так как фильмов каждый
+              месяц выходит порядка сотни, то зачастую в них тяжело отыскать действительно стоящие фильмы: настоящие киношедевры
+               и будущие блокбастеры. Поэтому, чтобы вам легко было ориентироваться и быть всегда в курсе о новинках российского
+                кинопроката, мы публикуем этот график самых знаковых кинопремьер в России, в котором вы найдете даты выхода самых
+                 ожидаемых и самых лучших фильмов, а также краткое их описание, главные подробности и лучшие трейлеры.</p>
+           <a href="/article.php">
+             <div class="more">Далее</div>
+           </a>
+       </div>
 		</div>
-		<div id="rightCol">
-			<div class="banner">
-				<img src="/img/banner_1.jpg" alt="Баннер 1" title="Баннер 1">
-				
-			
-			</div>
-			<div class="banner">
-				<img src="/img/banner_2.jpg" alt="Баннер 2" title="Баннер 2">
-				
-			
-			</div>
-		
-		</div>
+    <?php require_once "blocks/rightCol.php" ?>
    </div>
    
-   <footer>
-		<div id="social">
-			<a href="http://vk.com" title="Группа Вк" target= "_blank">
-				<img src="/img/vk.png" alt="Вк" title="Вк">
-			</a>
-			<a href="http://facebook.com" title="Группа Facebook" target= "_blank">
-				<img src="/img/facebook.png" alt="Facebook" title="Facebook">
-			</a>
-			<a href="http://twitter.com" title="Группа Twitter" target= "_blank">
-				<img src="/img/twitter.png" alt="Twitter" title="Twitter">
-			</a>
-		</div>
-		<div id="rights">
-			Все права защищены &copy;<?php echo date('Y')?>
-		</div>
-   
-   </footer>
+   <?php require_once "blocks/footer.php" ?>
 </body>
 </html>
